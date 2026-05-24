701d54ead3daaea92b759938df50bd4a5d5bf7f8
Michael Ringhus Gertz <michael@ringhus.dk>
2026-05-24 12:25:25 +0200

    fix(test): added h2 to testing

--- src/test/java/dk/viplev/targetapp/ApiApplicationTests.java
+++ src/test/java/dk/viplev/targetapp/ApiApplicationTests.java
@@ -2,8 +2,10 @@ package dk.viplev.targetapp;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("test")
 class ApiApplicationTests {
 
 	@Test
@@ -11,3 +13,4 @@ class ApiApplicationTests {
 	}
 
 }
+lg
