990220a669fc49206803a1635737923fb9d9c1c4
Michael Ringhus Gertz <michael@ringhus.dk>
2026-05-24 12:06:59 +0200

    fix(test): renamed test file

--- src/test/java/dk/viplev/targetapp/ApiApplicationTests.java
+++ src/test/java/dk/viplev/targetapp/ApiApplicationTests.java
@@ -4,7 +4,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class TargetappApplicationTests {
+class ApiApplicationTests {
 
 	@Test
 	void contextLoads() {
