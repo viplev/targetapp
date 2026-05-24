ebe3fb09646aeb50f1f114dd32a98274ca4a22af
Michael Ringhus Gertz <michael@ringhus.dk>
2026-05-24 12:58:26 +0200

    feat(rest_controller): added forward from / to /swagger-ui/

---
+++ src/main/java/dk/viplev/targetapp/inbound/rest/RootController.java
@@ -0,0 +1,19 @@
+package dk.viplev.targetapp.inbound.rest;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.view.RedirectView;
+
+import io.swagger.v3.oas.annotations.Hidden;
+
+@RestController
+@RequestMapping("/")
+public class RootController {
+    
+    @Hidden
+    @GetMapping
+    public RedirectView redirectToSwagger() {
+        return new RedirectView("/swagger-ui/index.html");
+    }
+}
