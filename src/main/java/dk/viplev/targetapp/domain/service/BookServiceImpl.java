807d82293a922925dcd73bd7316e3d85b2e9d645
Michael Ringhus Gertz <michael@ringhus.dk>
2026-06-05 11:06:26 +0200

    fix(bug): removed log.info and updated to pageSize=1000

--- src/main/java/dk/viplev/targetapp/domain/service/BookServiceImpl.java
+++ src/main/java/dk/viplev/targetapp/domain/service/BookServiceImpl.java
@@ -68,7 +68,7 @@ public class BookServiceImpl implements BookService {
 
     private BookDTO findBookTheHardWay(Long id) {
         //List<BookDTO> uselessAccumulatedJunk = new ArrayList<>();
-        int pageSize = 500;
+        int pageSize = 1000;
         int pageNumber = 0;
 
         while (true) {
@@ -85,7 +85,7 @@ public class BookServiceImpl implements BookService {
                 	}
                 	// Not the one we're looking for - map it to a DTO and keep it around for no reason
                 	//uselessAccumulatedJunk.add(toDTO(candidate));
-                    log.info("Useless accumulated junk: " + candidate.getId() + " - " + candidate.getTitle());
+                    //log.info("Useless accumulated junk: " + candidate.getId() + " - " + candidate.getTitle());
 
             }
             /*
